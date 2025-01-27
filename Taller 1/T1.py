d1e0938a19c58b9f5284ff762402677ba9355beb
slaverdeg <s.laverdeg@uniandes.edu.co>
2025-01-27 13:51:30 -0500

    1c)

--- Taller 1/T1.py
+++ Taller 1/T1.py
@@ -5,7 +5,7 @@ import pandas as pd
 def prom(array):
     return np.sum(array) / np.size(array)
 
-df1 = pd.read_csv(r"C:\Users\ASUS\Downloads\Rhodium.csv")
+df1 = pd.read_csv("Taller 1\Rhodium.csv")
 
 array_wave = df1['Wavelength (pm)'].to_numpy(); array_inten = df1['Intensity (mJy)'].to_numpy()
 array_wave = np.append(array_wave, array_wave[-1]); array_inten = np.append(array_inten, array_inten[-1])
@@ -73,33 +73,62 @@ I = np.delete(array_inten_new,slice(255,410))
 coeficientes, covarianza = np.polyfit(w, I, 16, cov=True)
 pol = GetModel(array_wave_new[255:410],coeficientes[::-1])
 
+espectro_x = array_wave_new
+espectro_y = np.concatenate((np.concatenate((array_inten_new[:255],pol)), array_inten_new[410:]))
+
+#print(len(array_inten_new[:255])+len(pol)+len(array_inten_new[410:]), len(espectro_y))
+
 pico1_max = np.max(array_inten_new[255:410] - pol)
 
 espectro_max = np.max(I)
 
 i = np.argmax(array_inten_new[255:410] - pol)
-n = 30
+n = 21
 pico1 = (array_inten_new[255:410] - pol)[i-n:i+n]
+pico1_x = (array_wave_new[255:410])[i-n:i+n]
 
 pico2_max = np.max((array_inten_new[255:410] - pol)[i+n:])
 j = np.argmax((array_inten_new[255:410] - pol)[i+n:])
-m = 38
+m = 25
 pico2 = ((array_inten_new[255:410] - pol)[i+n:])[j-m:j+m]
+pico2_x = ((array_wave_new[255:410])[i+n:])[j-m:j+m]
 
-print(espectro_max,pico1_max,pico2_max)
 
-def FWHM(array_x,array_y):
+"""def FWHM(array_x,array_y):
     x = array_x
     y = array_y
     i = np.argmax(y)
-    x_bar = 1
+    x_bar = x[i]
+    d = x - x_bar
+    sigma = np.sqrt(np.sum(d**2)/(len(x)-1))
+    return 2*np.sqrt(2*np.log(2))*sigma
+    
+print(FWHM(pico1_x,pico1),FWHM(pico2_x,pico2))"""
 
-#plt.scatter(array_wave_new[255:410],pol,marker='+',color= 'red')
-#plt.scatter(w,I,marker='+',color= 'red')
-#plt.scatter(array_wave_new[255:410],array_inten_new[255:410],marker='x',color= 'green')
+def anchura(array_x,array_y):
+    x = array_x
+    y = array_y
+    max = np.max(y)
+    pos = np.argmax(y)
+    for i in range(pos):
+        if y[i] <= max/2:
+            x1 = x[i]
+    for i in range(len(x)-pos):
+        if y[-i] <= max/2:
+            x2 = x[-i]
+    return x2-x1
 
-plt.scatter(array_wave_new[255:410], array_inten_new[255:410] - pol)
-plt.scatter((array_wave_new[255:410])[i-n:i+n], pico1,marker='x',color= 'red')
-plt.scatter(((array_wave_new[255:410])[i+n:])[j-m:j+m], pico2,marker='x',color= 'red')
+
+print(espectro_max,pico1_max,pico2_max)
+print(anchura(espectro_x,espectro_y),anchura(pico1_x,pico1),anchura(pico2_x,pico2))
+
+
+#plt.plot(array_wave_new, GetModel(array_wave_new,coeficientes[::-1])) #modelo
+plt.plot(array_wave_new,array_inten_new) #datos filtraos
+plt.plot(espectro_x,espectro_y,color= 'red') #espectro
+plt.plot(array_wave_new,array_inten_new - GetModel(array_wave_new,coeficientes[::-1]),color= 'green') #picos
+
+#plt.scatter(pico1_x, pico1,marker='x',color= 'red')
+#plt.scatter(pico2_x, pico2,marker='x',color= 'red')
 
 plt.show()
